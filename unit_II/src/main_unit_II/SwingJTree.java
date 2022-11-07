88c39d61a24ec620ca3d2b6abe2f10676d94c817
sparutwar <sparutwar@gmail.com>
2022-11-07 15:00:31 +0530

    add

---
+++ unit_II/src/main_unit_II/SwingJTree.java
@@ -0,0 +1,15 @@
+package main_unit_II;
+
+import javax.swing.JFrame;
+import javax.swing.JTree;
+
+public class SwingJTree {
+	public static void main(String[] args) {
+		JFrame jf = new JFrame("Swing JComboBox");
+		JTree jt = new JTree();
+		
+		jf.setVisible(true);
+		jf.setSize(700, 700);
+		jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+}
